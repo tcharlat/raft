de5b45759a54f0f0d93f473704e2fb740de8795a
Thomas Charlat <thomas.charlat@gmail.com>
2015-07-17 16:11:48 +0200

    heavy refactoring

--- raft.js
+++ raft.js
@@ -5,196 +5,108 @@
 	else env.raft = raft();
 
 	function raft() {
-
-		return classFactory;
-
-		function classFactory(model) {
-			var _arraysPromise = {};
-			
-
+		return function classFactory(model) {
+			var _reserved = [
+				'toJSON',
+				'save',
+				'update',
+				'load',
+				'create',
+				'find'
+			];
 			/* CONSTRUCTOR */
 			var Class = function(values) {
-				var self = this;
-
-				/*
-				** defines a getter setter for each variable in the model attributes : {}
-				** if a fields had a default value define in the model attributes default,
-				** sets the get-set to this value
-				*/
-
-				/*
-				** Since some logic has to be executed once at the creation of the constructor (sic),
-				** we should check that we are not executing unnecessary redundant construction instructions
-				** and kick some loops out of the constructor
-				*/
 				values = values || {};
-				for (var name in model.attributes) {
-					self[name] = getterSetter(values[name] || model.attributes[name].default);
-					/*
-					** defines a subfunction attached to the getter-setter for each method in
-					** the model.attributes.is : {}
-					*/
-					for (var method in model.attributes[name].is) {
-						if (self[name].is == null)
-							self[name].is = {};
-						self[name].is[method] = isMethod (
-							name,
-							method,
-							model.attributes[name].is[method],
-							self[name]
-						);
-						if (_arraysPromise[method] == null)
-							_arraysPromise[method] = [];
-						_arraysPromise[method].push(self[name].is[method]);
-
-						/**
-						** this big fat ugly closure should be externalised for clarity !
-						** for each instance.gettersetter.is.method() that is defined at least once,
-						** creates an instance method that returns a promise fiwn
-						** instance
-						*/
-						Class.prototype.is[method] = (function(methodName) {
-							return function() {
-								return Promise.all(_arraysPromise[methodName].map(function (promise){
-									return promise();
-								}));
-							};
-						})(method);
-					}
-
-				}
-				/*
-				** defines an instance.method() for each variable in the model instanceMethods : {}
-				*/
-				for (var name in model.instanceMethods) {
-					self[name] = model.instanceMethods[name];
-				}
-
-				/*
-				** If the model has a field that will be used as a save ID, defines a _id() function that
-				** returns the custom identifier for this instance.
-				** Else, the class is a singleton and is stored/id'd under the prefix key only.
-				*/
-				if (model.id) {
-					self._id = function _id () {
-						return model.prefix + '/'+ self[model.id]();
-					};
-				}
-				else
-					self._id = function _id () {
-						return model.prefix;
-					};
+				for (var name in model.attributes)
+					this['_'+name] = values[name] || model.attributes[name].default;	
 			};
+			/*! CONSTRUCTOR !*/
+			/*  VARIABLES  */
+			var _isPromises = {};
+			/*! VARIABLES !*/
+			/*  ATTRIBUTES  */
+			if (model.attributes) {
+				for (var name in model.attributes) {
+					/*  GETTER SETTER  */
+					Class.prototype[name] = getterSetter(name);
+					/*! GETTER SETTER !*/
+					/*  IS  */
+					for (var method in model.attributes[name].is) {
+						var _nameIsMethod = [name, 'Is', method].join('');
+						/*  ATTRIBUTE IS METHOD  */
+						Class.prototype[_nameIsMethod] = attributeIsMethod(name, method);
+						/*! ATTRIBUTE IS METHOD !*/
+						/*  INSTANCE IS METHOD PUBLISH  */
+						if (_isPromises[method] == null)
+				 			_isPromises[method] = [];
+				 		_isPromises[method].push(Class.prototype[_nameIsMethod]);
+				 		/*!  INSTANCE IS METHOD PUBLISH !*/
+					}/*! IS !*/
+				}
+				/*  INSTANCE IS METHOD SUBS  */
+				for (var method in _isPromises)
+					Class.prototype[['is', method].join('')] = isMethod(_isPromises[method]);
+				/*! INSTANCE IS METHOD SUBS !*/
 
-			/* !CONSTRUCTOR! */
-
-			/*
-			** Might not be the best way to manage a closure. We should definitely audit this at some point
-			*/
-			function isMethod (varName, methodName, method, getterSetter) {
-				return (function (_varName, _methodName, _method, _getterSetter) {
+				/*  ATTRIBUTES FUNCTIONS  */
+				function isMethod (promiseArray) {
 					return function () {
+						var self = this;
+						return Promise.all(promiseArray.map(function (promise) {
+							return promise.bind(self)();
+						}));
+					};
+				}
+				function attributeIsMethod (varName, methodName) {
+					return function () {
+						var self = this;
 						return new Promise(function (resolve, reject) {
-							if (_method.bind(null, _getterSetter)())
-								resolve(_getterSetter);
+							if (model.attributes[varName].is[methodName].bind(self, Class.prototype[varName].bind(self))())
+								resolve(Class.prototype[varName].bind(self));
 							else
-								reject(model.prefix + '/' + varName + ': \'' + _getterSetter() + '\' is not ' + methodName);
+								reject(model.prefix + '/' + varName + ': \'' + self[varName]() + '\' is not ' + methodName);
 						});
 					};
-				})(varName, methodName, method, getterSetter);
-			}
-
-			/*
-			** TODO :
-			** This object should be created only if there is at least one if method
-			*/
-			Class.prototype.is = {};
-
-			/* INSTANCE METHODS */
-
-			Class.prototype.update = function (obj) {
-				for(var name in model.attributes) {
-					if (obj[name])
-						this[name](obj[name]);
 				}
-			};
-			Class.prototype.toJSON = function () {
-				var _obj = {};
-				for(var name in model.attributes) {
-					_obj[name] = this[name]();
+				function getterSetter(varName) {
+					return function(/*value*/) {
+						if (arguments.length)
+							this['_'+varName] = arguments[0];
+						return this['_'+varName];
+					};
 				}
-				return JSON.stringify(_obj);
-			};
+				/*! ATTRIBUTES FUNCTIONS !*/
+			}/*! ATTRIBUTES !*/
+			/*  COLLECTIONS  */
+			if (model.collections) {
+				for (var collection in model.collections) {
 
-			/*
-			** This might be better defined as a custom model instance method
-			*/
-			Class.prototype.current = function current () {
-				Class.current = this;
-			};
+				}
+			}/*! COLLECTIONS !*/
+			/*  OBJECTS  */
+			if (model.objects) {
+				for (var object in model.objects) {
 
-			/* ! INSTANCE METHODS ! */
-			/*
-			** Idem for this variable
-			*/
-			Class.current;
+				}
+			}/*! OBJECTS !*/
+			/*  INSTANCE METHODS  */
+			if (model.methods) {
+				for (var method in model.methods) {
 
-			/* INSTANCE PRIVATE */
+				}
+			}/*! INSTANCE METHODS !*/
+			/*  CLASS METHODS  */
+			if (model.statics) {
+				for (var method in model.statics) {
 
-			/*
-			** This function definition is highly unclear + it's not implemented.
-			** should be solved ASAP
-			*/
-			function load () {};
-
-			/* !INSTANCE PRIVATE! */
-
-			/* STATIC METHODS */
-
-			/*
-			** defines a class.method() for each method in classMethods {}
-			*/
-			for (var name in model.classMethods)
-			{
-				Class[name] = model.classMethods[name];
-			}
-
-			Class.create = function create(attributes) {
-				return new Promise(function(resolve) {
-					/*
-					** Should async creation fail ? under wich condition ?
-					*/
-					//if (someCheckIsNotPassing(attributes) || somethingIsNotRight())
-					//	reject('clever error message');
-					//else
-					resolve(new Class(attributes));
-				});
-			};
-
-			/*
-			** TODO :
-			** Should go in localStorage decorator
-			*/
-			Class.find = function find () {
-				//returns an array
-			};
-
-			/* !STATIC METHODS! */
-
+				}
+			}/*! CLASS METHODS !*/
+			/*  DEFAULT INSTANCE METHOD  */
+			/*! DEFAULT INSTANCE METHOD !*/
+			/*  DEFAULT CLASS METHOD  */
+			/*! DEFAULT CLASS METHOD !*/
+			/* KEEP AT THE END */
 			return Class;
-		}
-
-		function getterSetter(store) {
-			var prop = function(/*value*/) {
-				if (arguments.length)
-					store = arguments[0];
-				return store;
-			};
-			return prop;
-		}
+		};
 	}
 }(this);
-
-// if (typeof module != "undefined" && module !== null && module.exports) module.exports = raft;
-// else if (typeof define === "function" && define.amd) define(function() {return raft});
-
